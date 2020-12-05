517caacd30cd55cf9b14e9ee2dde37375a5ac718
salmaeidvelasco <a2019115131@estudiantes.upsa.edu.bo>
2020-12-05 13:38:09 -0400

    Se agregó el controlador con los especificado, todo está completo.

---
+++ OperacionesNumero/OperacionesNumero/Controllers/NumeroController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OperacionesNumero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumeroController : ControllerBase
+    {
+
+        //GET
+        [HttpGet]
+        public string OperacionGet(int numero)
+        {
+            if (numero < 0)
+            {
+                return "Error";
+            }
+            else { }
+
+            if (numero == 0)
+            {
+                return "Realizado por Salma Eid";
+            }
+            else { }
+            if (numero > 0)
+            {return "Usted ingresó el número"; 
+            
+            }
+            else { }
+                return "No existe esa acción";
+        }
+
+        //POST
+        [HttpPost]
+        public string OperacionPost([FromHeader]int numero)
+        {
+            if (numero < 0)
+            {
+                return "Error";
+            }
+            else { }
+
+            if (numero == 0)
+            {
+                return "Realizado por Salma Eid";
+            }
+            else { }
+            if (numero > 0)
+            {
+                return "Usted ingresó el número";
+
+            }
+            else { }
+            return "No existe esa acción";
+        }
+
+
+
+
+    }
+}
